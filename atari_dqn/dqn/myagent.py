a9c94bb1fa78975beee4a4d96828b3a4a8799483
Parag Gupta <paraggupta1993@gmail.com>
2016-12-15 07:04:15 -0500

    something something

--- atari_dqn/dqn/myagent.py
+++ atari_dqn/dqn/myagent.py
@@ -11,17 +11,56 @@ class Agent(BaseModel):
         self.sess = sess
         self.env = environment
         self.memory = ReplayMemory(self.config, self.model_dir)
-
         self.build_dqn()
 
     def train(self):
-        ## Called when just training the model
+        ## new game
+        screen, reward, action, terminal = self.env.new_random_game()
 
-        ## Predict action
+        ## populate history with same screen
+        for _ in range(self.history.length):
+            self.history.add(screen)
 
-        ## train NN with sample events
+        ## Take some random steps
+        for self.step in tqdm(range(start_step, self.learn_step), ncols=70, initial=start_step):
 
-        ## Get state and Feed Replay Memory
+            action = self.predict(self.history.get(), random_action=True)
+            screen, reward, terminal = self.env.act(action, is_training=True)
+
+            self.history.add(screen)
+            self.memory.add(screen, reward, action, terminal)
+
+
+        for self.step in tqdm(range(learn_step, self.max_step), ncols=70, initial=learn_step):
+
+            ## Predict action
+            action = self.predict(self.history.get(), random_action=True)
+            screen, reward, terminal = self.env.act(action, is_training=True)
+
+            self.history.add(screen)
+            self.memory.add(screen, reward, action, terminal)
+
+            ## train NN with sample events
+
+            self.minibatching()
+
+    def minibatching():
+        stateT action, reward, stateT1, terminal = self.memory.sample()
+
+        ##
+
+    def predict(self, state, random_action):
+        ep = (self.ep_end +
+                    max(0., (self.ep_start - self.ep_end)
+                                  * (self.ep_end_t - max(0., self.step - self.learn_start)) / self.ep_end_t))
+
+        if random_action or random.random() < ep:
+            action = random.randrange(self.env.action_size)
+        else:
+            pass
+            ##action = ##
+
+        return action
 
 
     def play(self):
@@ -36,7 +75,9 @@ class Agent(BaseModel):
 
         ## Pixels of recent 4 frames
         self.state = tf.placeholder('float32',
-                [None, self.history_length, self.screen_height, self.screen_width], name='state']
+                [None, self.screen_height, self.screen_width, self.history_length], name='state']
+
+
 
 
 

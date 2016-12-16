1d0042864f3302c80e3ff54cd4dfbc7c0098cdb2
Parag Gupta <paraggupta1993@gmail.com>
2016-12-16 00:09:52 -0500

    working myagent

--- atari_dqn/dqn/myagent.py
+++ atari_dqn/dqn/myagent.py
@@ -5,15 +5,18 @@ from .base import BaseModel
 from .history import History
 from .replay_memory import ReplayMemory
 import random
+import numpy as np
 class MyAgent(BaseModel):
 
     def __init__(self, config, environment, sess):
         super(MyAgent, self).__init__(config)
         self.sess = sess
         self.env = environment
+        self.best_reward = 0.0
         self.history = History(self.config)
         self.memory = ReplayMemory(self.config, self.model_dir)
         self.build_dqn()
+
     def train(self):
         ## new game
         screen, reward, action, terminal = self.env.new_random_game()
@@ -31,26 +34,39 @@ class MyAgent(BaseModel):
             self.history.add(screen)
             self.memory.add(screen, reward, action, terminal)
 
+            ## new game
+            screen, reward, action, terminal = self.env.new_random_game()
+
+        episode_reward = 0
+
         ## Take steps and learn
         for self.step in tqdm(range(int(self.config.learn_start), int(self.config.max_step)), ncols=70, initial=int(self.config.learn_start)):
 
             ## Predict action
             action = self.predict(self.history.get(), random_action=True)
             screen, reward, terminal = self.env.act(action, is_training=True)
+            episode_reward += reward
+            if episode_reward > self.best_reward:
+                print "Best Reward: ", episode_reward
+                self.best_reward = episode_reward
 
             self.history.add(screen)
             self.memory.add(screen, reward, action, terminal)
 
-            ## train NN with sample events
+            if terminal:
+                ## new game
+                episode_reward = 0
+                screen, reward, action, terminal = self.env.new_random_game()
 
-            self.minibatching()
+            ## train NN with sample events
+            if self.step % self.train_frequency == 0:
+                self.minibatching()
 
     def minibatching(self):
         # FF state to find Q(s,a)
         # FF all s' to find max Q(s', a')
         # calculate target
         stateT, action, reward, stateT1, terminal = self.memory.sample()
-
         qT1 = self.q.eval({self.state: stateT1})
         terminal = np.array(terminal) + 0.
         maxqT1 = np.max(qT1, axis=1)
@@ -84,10 +100,7 @@ class MyAgent(BaseModel):
         ## None == 32 for minibatches of 32 states
 
         ## Pixels of recent 4 frames
-        self.state = tf.placeholder(shape=[None, 84, 84,4], dtype=tf.uint8, name='state')
-        self.state = tf.to_float(self.state) / 255.0
-        # The TD target value
-        self.y_pl = tf.placeholder(shape=[None], dtype=tf.float32, name="y")
+        self.state = tf.placeholder(shape=[None, 84, 84,4], dtype=tf.float32, name='state')
 
         ## convolutions
         conv1 = tf.contrib.layers.conv2d(self.state, 32, 8, 4, activation_fn=tf.nn.relu)
@@ -104,9 +117,14 @@ class MyAgent(BaseModel):
 
         self.q_action = tf.argmax(self.q, dimension=1)
 
+        #### ---- Action for a state done --- ###
+
+         # The TD target value
+        self.y_pl = tf.placeholder(shape=[None], dtype=tf.float32, name="y")
 
         ## Place holder for which actions was taken in minibatch samples
         self.action_pl = tf.placeholder(shape=[None], dtype=tf.int32, name='actions')
+
 
         ## Get the predictions for the chosen action only
         gather_indices = tf.range(self.batch_size) * tf.shape(self.q)[1] + self.action_pl
@@ -116,8 +134,8 @@ class MyAgent(BaseModel):
         self.losses = tf.squared_difference(self.y_pl, self.action_prediction)
 
         self.loss = tf.reduce_mean(self.losses)
-
+        print "loss:", self.loss
         self.optimizer = tf.train.RMSPropOptimizer(0.00025, 0.99, 0.0, 1e-6)
         self.train_op = self.optimizer.minimize(self.loss, global_step=tf.contrib.framework.get_global_step())
 
-
+        tf.initialize_all_variables().run()
